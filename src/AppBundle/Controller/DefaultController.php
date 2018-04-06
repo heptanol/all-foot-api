4df2f3605722341c8840026c21e15aaa449576bc
alaeddine thamine <alaeddine.thamine.prestataire@sfr.com>
2018-04-06 19:25:09 +0200

    Fixtures controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,6 +12,20 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     /**
+     * @Route("/api/competitions/{id}/fixtures", name="fixtures")
+     */
+    public function getFixturesAction(Request $request, $id)
+    {
+        $response = $this->get(ApiService::class)->getFixtures($id, $request->query->all());
+
+        $responseApi = new Response($response, JsonResponse::HTTP_OK);
+        $responseApi->headers->set('Content-Type', 'application/json');
+        $responseApi->headers->set('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+        return $responseApi;
+    }
+
+    /**
      * @Route("/api/{requestUrl}", name="api", requirements={"requestUrl"=".+"})
      */
     public function apiCallAction(Request $request, $requestUrl)
@@ -24,4 +38,5 @@ class DefaultController extends Controller
 
         return $responseApi;
     }
+
 }
--- src/AppBundle/Service/ApiService.php
+++ src/AppBundle/Service/ApiService.php
@@ -19,9 +19,15 @@ class ApiService
      */
     private $cache;
 
-    public function __construct(Client $client)
+    /**
+     * @var array
+     */
+    private $url;
+
+    public function __construct(Client $client, array $url)
     {
         $this->client = $client;
+        $this->url = $url;
         $this->cache = new FilesystemCache('', 60);
     }
 
@@ -32,7 +38,7 @@ class ApiService
      */
     public function call(Request $request, $requestUrl)
     {
-        $url = '/v1/'. $requestUrl .'?'. $request->getQueryString();
+        $url = '/v1/' . $requestUrl . '?' . $request->getQueryString();
         $key = str_replace('/', '.', $url);
         $callNumber = $this->cache->has('count') ? $this->cache->get('count') : 0;
 
@@ -42,6 +48,44 @@ class ApiService
             $result = (string) $this->client->get($url)->getBody();
             $this->cache->set($key, $result);
             $this->cache->set('count', ++$callNumber);
+        }
+        return $result;
+    }
+
+    public function getFixtures($id, array $query)
+    {
+        $url = str_replace('{competitions_id}', $id, $this->url['get_fixtures']);
+        $key = $this->generateKey($url);
+
+        if ($this->cache->has($key)) {
+            $result = $this->cache->get($key);
+        } else {
+            $result = json_decode($this->client->get($url)->getBody());
+            $this->cache->set($key, $result);
+        }
+
+        if (property_exists($result, 'fixtures') && array_key_exists('matchday', $query)) {
+            $result = $this->filterByMatchDay($result->fixtures, $query['matchday']);
+        }
+
+        return json_encode($result);
+    }
+
+    private function generateKey($url)
+    {
+        return str_replace('/', '.', $url);
+    }
+
+    private function filterByMatchDay(array $fixtures, $matchDay)
+    {
+        $result = array();
+        foreach ($fixtures as $fixture) {
+            if ($fixture->matchday == $matchDay) {
+                $day = new \DateTime($fixture->date);
+                $day->setTime(0,0,0);
+                $fixture->day = $day->format('Y-m-d\TH:i:s\Z');
+                $result[] = $fixture;
+            }
         }
         return $result;
     }
